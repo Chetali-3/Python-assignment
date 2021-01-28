1c37eb77cab7902f652c55a71dc037fd358a1c21
github chetali-3 <vchetalibhatt@gmail.com>
2021-01-28 10:16:26 +0000

    add changes

--- hello.py
+++ hello.py
@@ -1,9 +1,9 @@
- Making a to-do management file.
+# Making a to-do management file.
 
 # Ask to operator
 def print_name(name) :
-    print("Hello" + " " + name)
-print_name(input("Please enter your name : "))
+    print("Hello"+ " " + name)
+print_name(input("Please enter your name :" ))
 
 # number of entries
 number_of_entries = input("How many entries do you wanna make : ")
@@ -12,14 +12,15 @@ number_of_entries = input("How many entries do you wanna make : ")
 fname = input("Please enter your file name with adding(.txt) : ")
 
 # Creating a file
-file = open( fname , "w")
+file = open(fname , "w")
 
 for i in range( 0, int(number_of_entries )) :
-     Task = input("Task : ")
-     entry = f"{ Task }\n"
-     if i == int(number_of_entries ) - 1 :
-         print("Done😄!")
-     else :
+    Task = input("Task : ")
+    entry = f"{Task}\n" 
+    file.write(entry)
+    if i == int(number_of_entries ) - 1 :
+         print("Done!")
+    else :
          print("Okay, next one:")
 file.close()
 
@@ -29,3 +30,5 @@ for line in x :
        count += 1
 print(count)
 
+#printing the file name
+print(fname)
